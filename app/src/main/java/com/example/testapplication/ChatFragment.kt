93f707b6deb6ac4a0d4d64935699bc6a8a5b3463
Daeun <eldms0406@naver.com>
2022-11-07 16:55:34 +0900

    fragment 구현

---
+++ app/src/main/java/com/example/testapplication/ChatFragment.kt
@@ -0,0 +1,32 @@
+package com.example.testapplication
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.testapplication.databinding.FragmentChatBinding
+
+class ChatFragment: Fragment() {
+    private var _binding : FragmentChatBinding? = null
+    private val binding : FragmentChatBinding
+        get() = requireNotNull(_binding)
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentChatBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
