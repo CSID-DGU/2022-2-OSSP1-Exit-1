b47bc502fd5deba8190880a533399a49f683b353
Daeun <eldms0406@naver.com>
2022-11-17 17:41:48 +0900

    채팅 리스트 페이지 1차 완성(디자인 x)

---
+++ app/src/main/java/com/example/testapplication/ChatlistAdapter.kt
@@ -0,0 +1,47 @@
+package com.example.testapplication
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.testapplication.databinding.LayoutChatlistBinding
+
+class ChatlistAdapter(context : Context): RecyclerView.Adapter<ChatlistAdapter.ChatlistViewHolder>() {
+
+    private val inflater by lazy { LayoutInflater.from(context)}
+    private var categorylist: List<ChatlistData> = emptyList()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChatlistAdapter.ChatlistViewHolder {
+        val binding = LayoutChatlistBinding.inflate(inflater, parent,false)
+        return ChatlistViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ChatlistAdapter.ChatlistViewHolder, position: Int) {
+        holder.bind(categorylist[position])
+
+        //holder.itemView.setOnClickListener {
+        //  Log.d("클릭","클릭")
+        //val intent = Intent(holder.itemView?.context, MatchingEnd::class.java)
+        //ContextCompat.startActivity(holder.itemView.context, intent,null)
+        //}
+    }
+
+    override fun getItemCount(): Int {
+        return categorylist.size
+    }
+
+    class ChatlistViewHolder(
+        private val binding: LayoutChatlistBinding
+    ): RecyclerView.ViewHolder(binding.root) {
+        fun bind(data: ChatlistData) {
+            binding.tvChatlistTitle.setText(data.chatlist_title)
+            binding.tvChatlistHashtag.setText(data.chatlist_hashtag)
+
+        }
+    }
+
+    fun setRepoList(repoList: List<ChatlistData>){
+        this.categorylist = repoList.toList()
+        notifyDataSetChanged()
+    }
+}
---
+++ app/src/main/java/com/example/testapplication/ChatlistData.kt
@@ -0,0 +1,6 @@
+package com.example.testapplication
+
+data class ChatlistData(
+    val chatlist_title : String,
+    val chatlist_hashtag: String,
+)
