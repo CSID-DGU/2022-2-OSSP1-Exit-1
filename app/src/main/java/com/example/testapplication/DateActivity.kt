6b472dbfb12da62d4d3a7b8781bf152642ecdfe6
Daeun <eldms0406@naver.com>
2022-11-26 19:59:21 +0900

    매칭페이지(날짜 페이지)수정
    edittext->datepickerdialog

--- app/src/main/java/com/example/testapplication/DateActivity.kt
+++ app/src/main/java/com/example/testapplication/DateActivity.kt
@@ -1,19 +1,33 @@
 package com.example.testapplication
 
+import android.app.DatePickerDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import com.example.testapplication.databinding.ActivityDatecbBinding
+import java.util.*
 
 class DateActivity : AppCompatActivity() {
     private lateinit var btn_next: Button
     private lateinit var btn_past: Button
+    var dateString = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityDatecbBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.tvStartdate.setOnClickListener {
+            val cal = Calendar.getInstance()    //캘린더뷰 만들기
+            val dateSetListener = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+                dateString = "${year}년 ${month+1}월 ${dayOfMonth}일"
+                binding.tvStartdate.text = "시작일 : " + dateString
+            }
+            DatePickerDialog(this, dateSetListener, cal.get(Calendar.YEAR),cal.get(Calendar.MONTH),cal.get(Calendar.DAY_OF_MONTH)).show()
+        }
+
+
 
         btn_next=findViewById(R.id.btn_next)
         btn_past=findViewById(R.id.btn_back)
