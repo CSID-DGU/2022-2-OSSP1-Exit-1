d8d72771da2381e04817584262150b244bafa0f9
DrRivaski <48974215+DrRivaski@users.noreply.github.com>
2022-11-27 05:04:01 +0900

    아이디 중복 확인 및 회원가입 기능 구현
    //TODO 로그인 기능 구현

---
+++ app/src/main/java/com/example/testapplication/APIS.kt
@@ -0,0 +1,44 @@
+package com.example.testapplication
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import retrofit2.http.Field
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Body
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Query
+
+
+public interface APIS {
+    @GET("checkExist.php")
+    fun checkExist(
+        @Query("id") param1: String? = null
+    ): Call<UserModel>
+
+    @FormUrlEncoded
+    @POST("signup.php")
+    fun signup(
+        @Field("id") id: String? = null,
+        @Field("password") password: String? = null,
+        @Field("name") name: String? = null
+    ): Call<UserPostModel>
+
+    companion object {
+        private const val BASE_URL =
+            "http://ec2-43-200-191-232.ap-northeast-2.compute.amazonaws.com/"
+
+        fun create(): APIS {
+            val gson: Gson = GsonBuilder().setLenient().create()
+
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build()
+                .create(APIS::class.java)
+        }
+    }
+}
