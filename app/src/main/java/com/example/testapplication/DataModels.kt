500cf37c5c2b7e5784f39908bbad4028b158ed59
DrRivaski <48974215+DrRivaski@users.noreply.github.com>
2022-12-18 16:44:24 +0900

    data models, APIS 구현

--- app/src/main/java/com/example/testapplication/DataModels.kt
+++ app/src/main/java/com/example/testapplication/DataModels.kt
@@ -40,3 +40,57 @@ data class UserSigninModel(
     @SerializedName("result")
     var result: String? = null
 )
+
+data class createRoomPostModel(
+    @Expose
+    @SerializedName("createrUserId")
+    var createrUserId: String? = null,
+
+    @Expose
+    @SerializedName("title")
+    var title: String? = null,
+
+    @Expose
+    @SerializedName("region1")
+    var region1: String? = null,
+
+    @Expose
+    @SerializedName("region2")
+    var region2: String? = null,
+
+    @Expose
+    @SerializedName("region3")
+    var region3: String? = null,
+
+    @Expose
+    @SerializedName("dateFrom")
+    var dateFrom: String? = null,
+
+    @Expose
+    @SerializedName("dateTo")
+    var dateTo: String? = null,
+
+    @Expose
+    @SerializedName("genre")
+    var genre: String? = null,
+
+    @Expose
+    @SerializedName("difficulty")
+    var difficulty: Int? = null,
+
+    @Expose
+    @SerializedName("fear")
+    var fear: Int? = null,
+
+    @Expose
+    @SerializedName("activity")
+    var activity: Int? = null,
+
+    @Expose
+    @SerializedName("roomIntro")
+    var roomIntro: String? = null,
+
+    @Expose
+    @SerializedName("result")
+    var result: String? = null
+)
