32a98de3b5b60dfbaec45061cd75608970d3ba99
Lee-JeongSoo <chaola3503@gmail.com>
2022-12-18 15:42:48 +0900

    임시저장

--- app/src/main/java/com/example/testapplication/TestActivity.kt
+++ app/src/main/java/com/example/testapplication/TestActivity.kt
@@ -2,10 +2,20 @@ package com.example.testapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RelativeLayout
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 
 class TestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_test)
+        setContentView(R.layout.drawer_navi)
+
+        //버튼을 눌러 메뉴를 오픈할 수도 있고, 왼쪽에서 오른쪽으로 스왑해 오픈할 수 있습니다.
+        //DrawerLayout의 id에 직접 openDrawer()메소드를 사용할 수 있습니다.
+        open_menu_btn.setOnClickListener {
+            main_drawer_layout.openDrawer((GravityCompat.START))
+        }
+
     }
 }
